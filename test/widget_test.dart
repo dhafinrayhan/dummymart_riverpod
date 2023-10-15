e50bd65be74334739746cdcbbac14d1b68ae3695
Dhafin Rayhan A <dhafindiamle@gmail.com>
2023-10-15 10:28:56 +0700

    Fix test error

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:dummymart/main.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
   testWidgets('App built and triggers a frame', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(DummyMartApp());
+    await tester.pumpWidget(ProviderScope(child: DummyMartApp()));
+    await tester.pumpAndSettle();
   });
 }
